8cef9a30629f9eac6b06c4519819b9bf61f3e165
gwon11225 <gwon11225@gmail.com>
2024-04-11 18:06:12 +0900

    Docs :: create sql template 데이터베이스 생성 및 유저 생성 SQL문 작성, 유저 테이블 생성, 속성 (기본키, 이메일, 이름, 생일, 비밀번호)

---
+++ src/main/resources/sql/table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
