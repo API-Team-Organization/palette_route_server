e50a6504d364521924b1cd0599a911c50848b790
gwon11225 <gwon11225@gmail.com>
2024-04-18 17:49:39 +0900

    Feat :: redis config 추가 redis operation 빈 생성

---
+++ src/main/kotlin/com/teamapi/palette/config/RedisConfig.kt
@@ -0,0 +1,26 @@
+package com.teamapi.palette.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+    @Bean
+    fun redisOperations(
+        factory: ReactiveRedisConnectionFactory
+    ) : ReactiveRedisOperations<String, String> {
+        val serializer = StringRedisSerializer()
+
+        val builder: RedisSerializationContext.RedisSerializationContextBuilder<String, String> =
+            RedisSerializationContext.newSerializationContext(StringRedisSerializer())
+
+        val context: RedisSerializationContext<String, String> = builder.value(serializer).build()
+
+        return ReactiveRedisTemplate(factory, context)
+    }
+}
