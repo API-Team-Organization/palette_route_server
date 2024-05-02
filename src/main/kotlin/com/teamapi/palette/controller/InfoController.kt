d9fe8da6a0b3df73a20e3bb4d6202679e443d9d7
gwon11225 <gwon11225@gmail.com>
2024-05-02 17:45:38 +0900

    Feat :: 유저 정보 수정

--- src/main/kotlin/com/teamapi/palette/controller/InfoController.kt
+++ src/main/kotlin/com/teamapi/palette/controller/InfoController.kt
@@ -1,8 +1,12 @@
 package com.teamapi.palette.controller
 
+import com.teamapi.palette.dto.user.UpdateRequest
+import com.teamapi.palette.response.Response
 import com.teamapi.palette.response.ResponseBody
 import com.teamapi.palette.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,4 +18,10 @@ class InfoController(
     @GetMapping("/me")
     fun myInfo() = userService.me()
         .map { ResponseBody.ok("유저 조회 성공", it) }
+
+    @PatchMapping("/me")
+    fun updateInfo(
+        @RequestBody request: UpdateRequest
+    ) = userService.update(request)
+        .thenReturn(Response.ok("유저 수정 성공"))
 }
--- src/main/kotlin/com/teamapi/palette/service/UserService.kt
+++ src/main/kotlin/com/teamapi/palette/service/UserService.kt
@@ -1,5 +1,6 @@
 package com.teamapi.palette.service
 
+import com.teamapi.palette.dto.user.UpdateRequest
 import com.teamapi.palette.dto.user.UserResponse
 import com.teamapi.palette.repository.UserRepository
 import com.teamapi.palette.response.ErrorCode
@@ -24,5 +25,26 @@ class UserService(
                 userRepository.findById(it)
                     .map { UserResponse.from(it) }
             }
+    }
+
+    fun update(request: UpdateRequest): Mono<Void> {
+        return sessionHolder
+            .current()
+            .flatMap {
+                Mono.justOrEmpty(it.getAttribute<Long>("user"))
+            }
+            .switchIfEmpty { Mono.error(CustomException(ErrorCode.INVALID_SESSION)) }
+            .flatMap {
+                userRepository.findById(it)
+            }
+            .flatMap {
+                userRepository.save(
+                    it.copy(
+                        username = request.username ?: it.username,
+                        birthDate = request.birthDate ?: it.birthDate
+                    )
+                )
+            }
+            .then()
     }
 }
