0c69e572b2588b3665a838eb65f7f0b9f012d4d5
gwon11225 <gwon11225@gmail.com>
2024-05-07 20:25:23 +0900

    Feat :: bad request, method not allowed 예외 처리

--- src/main/kotlin/com/teamapi/palette/handler/ErrorHandler.kt
+++ src/main/kotlin/com/teamapi/palette/handler/ErrorHandler.kt
@@ -3,6 +3,7 @@ package com.teamapi.palette.handler
 import com.teamapi.palette.response.ErrorCode
 import com.teamapi.palette.response.Response
 import com.teamapi.palette.response.exception.CustomException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
 import org.springframework.web.HttpMediaTypeNotSupportedException
@@ -10,8 +11,11 @@ import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+import org.springframework.web.server.MethodNotAllowedException
+import org.springframework.web.server.ServerWebInputException
 
 @Suppress("unused")
 @RestControllerAdvice
@@ -39,4 +43,14 @@ class ErrorHandler {
     @ExceptionHandler(MethodArgumentTypeMismatchException::class)
     private fun handleMethodArgumentTypeMismatchException(e: MethodArgumentTypeMismatchException) =
         Response.of(ErrorCode.INVALID_PARAMETER.statusCode, ErrorCode.INVALID_PARAMETER.message)
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ServerWebInputException::class)
+    private fun badRequestException(e: ServerWebInputException) =
+        Response.of(ErrorCode.BAD_REQUEST.statusCode, ErrorCode.BAD_REQUEST.message)
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(MethodNotAllowedException::class)
+    private fun methodNowAllowedException(e: MethodNotAllowedException) =
+        Response.of(ErrorCode.METHOD_NOT_ALLOWED.statusCode, ErrorCode.METHOD_NOT_ALLOWED.message)
 }
