b216cd0faef54d35338d9bdd26c2326cf616d2a4
gwon11225 <gwon11225@gmail.com>
2024-06-12 13:54:49 +0900

    Fix :: request mapping 추가

--- src/main/kotlin/com/teamapi/palette/controller/RoomController.kt
+++ src/main/kotlin/com/teamapi/palette/controller/RoomController.kt
@@ -3,9 +3,11 @@ package com.teamapi.palette.controller
 import com.teamapi.palette.response.Response
 import com.teamapi.palette.service.RoomService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/room")
 class RoomController (
     private val roomService: RoomService
 ) {
