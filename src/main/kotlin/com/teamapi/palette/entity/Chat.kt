71e82e3ba2ab6587469c610d9d460456a85daa3a
gwon11225 <gwon11225@gmail.com>
2024-06-13 19:56:35 +0900

    Feat :: chat 엔티티 추가

--- src/main/kotlin/com/teamapi/palette/entity/Chat.kt
+++ src/main/kotlin/com/teamapi/palette/entity/Chat.kt
@@ -1,9 +1,18 @@
 package com.teamapi.palette.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import java.time.Instant
 
 data class Chat(
     @Id
     val id: Long? = null,
-    val message: String
+    val message: String,
+    val datetime: Instant,
+    @Column("room_id")
+    val roomId: Long,
+    @Column("user_id")
+    val userId: Long,
+    @Column("is_ai")
+    val isAi: Boolean
 )
