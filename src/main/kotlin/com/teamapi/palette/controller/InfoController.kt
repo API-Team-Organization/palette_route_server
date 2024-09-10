95780680bb78af7ced92c74eb2d27516abd486c8
jombi <jombi@duck.com>
2024-09-10 11:05:26 +0900

    fix :: missed controller edition

--- src/main/kotlin/com/teamapi/palette/controller/InfoController.kt
+++ src/main/kotlin/com/teamapi/palette/controller/InfoController.kt
@@ -2,9 +2,11 @@ package com.teamapi.palette.controller
 
 import com.teamapi.palette.annotations.SwaggerRequireAuthorize
 import com.teamapi.palette.dto.user.UpdateRequest
+import com.teamapi.palette.dto.user.UserResponse
 import com.teamapi.palette.response.Response
 import com.teamapi.palette.response.ResponseBody
 import com.teamapi.palette.service.UserService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -14,12 +16,16 @@ class InfoController(
     private val userService: UserService
 ) {
     @GetMapping("/me")
-    fun myInfo() = userService.me()
-        .map { ResponseBody.ok("유저 조회 성공", it) }
+    suspend fun myInfo(): ResponseEntity<ResponseBody<UserResponse>> {
+        val data = userService.me()
+        return ResponseBody.ok("유저 조회 성공", data)
+    }
 
     @PatchMapping("/me")
-    fun updateInfo(
+    suspend fun updateInfo(
         @RequestBody request: UpdateRequest
-    ) = userService.update(request)
-        .thenReturn(Response.ok("유저 수정 성공"))
+    ): ResponseEntity<Response> {
+        userService.update(request)
+        return Response.ok("유저 수정 성공")
+    }
 }
