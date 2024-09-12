c6c69eb119a1ec5f54a16f7168a8254ad063d501
jombi <jombi@duck.com>
2024-09-12 14:36:32 +0900

    feat :: jOOQ extensions

---
+++ src/main/kotlin/com/teamapi/palette/repository/JooqHelper.kt
@@ -0,0 +1,14 @@
+package com.teamapi.palette.repository
+
+import kotlinx.coroutines.reactor.awaitSingle
+import org.jooq.Record
+import org.jooq.Record1
+import org.jooq.RecordMapper
+import org.jooq.ResultQuery
+import reactor.core.publisher.Flux
+
+suspend fun <E, R : Record1<E>> ResultQuery<R>.awaitAll(): List<E> =
+    Flux.from(this).map { it.value1() }.collectList().awaitSingle()
+
+suspend fun <T, R : Record> ResultQuery<R>.awaitAll(mapper: RecordMapper<in R, T>): List<T> =
+    Flux.from(this).mapNotNull { mapper.map(it)!! }.collectList().awaitSingle()
