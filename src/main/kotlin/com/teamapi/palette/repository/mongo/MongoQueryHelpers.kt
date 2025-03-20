f041040d5273581ee4477a68a19a83fd83bec6c3
jombi <jombi@duck.com>
2025-03-20 19:59:59 +0900

    fix :: dotPath can't track SerialName

--- src/main/kotlin/com/teamapi/palette/repository/mongo/MongoQueryHelpers.kt
+++ src/main/kotlin/com/teamapi/palette/repository/mongo/MongoQueryHelpers.kt
@@ -5,9 +5,55 @@ import com.mongodb.client.model.BsonField
 import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Sorts
 import com.mongodb.kotlin.client.coroutine.MongoCollection
+import kotlinx.serialization.SerialName
 import org.bson.conversions.Bson
-import org.springframework.data.mapping.toDotPath
+import kotlin.reflect.KProperty
 import kotlin.reflect.KProperty1
+import kotlin.reflect.full.findAnnotation
+
+private class KPropertyPath<T, U>(
+    val parent: KProperty<U?>,
+    val child: KProperty1<U, T>
+) : KProperty<T> by child
+
+/**
+ * Recursively construct field name for a nested property.
+ * @author Tjeu Kayim
+ */
+internal fun asString(property: KProperty<*>): String {
+    return when (property) {
+        is KPropertyPath<*, *> ->
+            "${asString(property.parent)}.${property.child.findRealName()}"
+        else -> property.findRealName()
+    }
+}
+
+internal fun KProperty<*>.findRealName() = findAnnotation<SerialName>()?.value ?: name
+
+/**
+ * Builds [KPropertyPath] from Property References.
+ * Refer to a nested property in an embeddable or association.
+ *
+ * For example, referring to the field "author.name":
+ * ```
+ * Book::author / Author::name isEqualTo "Herman Melville"
+ * ```
+ * @author Tjeu Kayim
+ * @author Yoann de Martino
+ * @since 2.5
+ */
+operator fun <T, U> KProperty<T?>.div(other: KProperty1<T, U>): KProperty<U> =
+    KPropertyPath(this, other)
+
+/**
+ * Extension for [KProperty] providing an `toPath` function to render a [KProperty] in dot notation.
+ *
+ * @author Mark Paluch
+ * @since 2.5
+ * @see PropertyPath.toDotPath
+ */
+fun KProperty<*>.toDotPath(): String = asString(this)
+
 
 infix fun <R, T : Any?> KProperty1<R, T>.eq(other: T?): Bson = Filters.eq(toDotPath(), other)
 infix fun <R, T> KProperty1<R, T>.ne(other: T?): Bson = Filters.ne(toDotPath(), other)
